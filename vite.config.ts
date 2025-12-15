32b376223002b27a3e3d973966157f1f9b417a4f
jeferson <jeferson@escape.ppg.br>
2025-12-15 08:50:17 -0300

    remove vite.

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "../../lgam_prod",
-  },
-});
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: "../../lgam_prod",
+//   },
+// });
